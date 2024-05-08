51dcc17309bff17b99eb84f7904effdc3456bd8a
Marco Antonio RS <mars@MacBook-Air-de-Marco.local>
2024-05-07 20:26:05 -0600

    Marco Ramon POSTS API NODE test

---
+++ app.js
@@ -0,0 +1,60 @@
+
+const {tap,map, mergeMap}  = require('rxjs/operators');
+const {from, forkJoin}  = require('rxjs');
+const {RxHR} = require ("@akanass/rx-http-request");
+var https = require('https');
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+ app.get('/posts',(req, res) => {
+   
+      RxHR.get('https://jsonplaceholder.typicode.com/posts')
+      .pipe(
+        map(datos => datos.body),
+        map(datos => JSON.parse(datos) ),
+      )
+      .subscribe(async (datos1) => {
+        let start = req.query.start;
+        let size = req.query.size
+        let x = Number(start) + Number(size);
+        let pp = datos1.slice(start, x);
+        let authors = [];
+        let comments = [];
+        let final = [];
+        pp.forEach(data => {
+            final.push(RxHR.get(`https://jsonplaceholder.typicode.com/users/${data.userId}`))
+            comments.push(RxHR.get(`https://jsonplaceholder.typicode.com/posts/${data.id}/comments`))
+        })
+        forkJoin(final).subscribe(autores => { 
+            forkJoin(comments).subscribe(comentarios => {
+                
+                let commentariosF = []
+                comentarios.forEach(comentario => {
+                    let comentA = JSON.parse(comentario.body);
+                    comentA.forEach(comentAs => {
+                        commentariosF.push(comentAs)
+                    })
+                 }
+                )
+                autores.forEach(autore => {
+                    let aut = JSON.parse(autore.body);
+                    authors.push(aut)
+                })
+                pp.forEach(p => {
+                    p.comment = commentariosF.filter(comentarioin => comentarioin.postId == p.id);
+                    p.autor =  authors.find(autor => p.userId == autor.id)
+                 })
+                res.send(pp);
+            })
+        })
+       
+        })
+    })
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
